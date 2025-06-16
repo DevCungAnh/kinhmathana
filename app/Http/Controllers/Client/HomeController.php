ce4619afef8fe10330eb72b65c620d23fcc043f0
DevCungAnh <anhntph51526@gmail.com>
2025-06-16 16:15:41 +0700

    fix edit slider

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -6,12 +6,17 @@ use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\News;
+use App\Models\Slider;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('client.home');
+        $sliders = Slider::where('is_active', true)
+            ->orderBy('sort_order', 'asc')
+            ->get();
+            
+        return view('client.home', compact('sliders'));
     }
 } 
